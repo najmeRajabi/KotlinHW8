ad0d6856031c9323ee673d49f951ba12093f5602
niha <niha8515@yahoo.com>
2022-02-28 03:27:00 -0800

    add ShowInfoActivity.kt and set binding

--- app/src/main/java/com/example/kotlinhw8/ShowInfoActivity.kt
+++ app/src/main/java/com/example/kotlinhw8/ShowInfoActivity.kt
@@ -2,13 +2,13 @@ package com.example.kotlinhw8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.kotlinhw8.databinding.ActivityMainBinding
+import com.example.kotlinhw8.databinding.ActivityShowInfoBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class ShowInfoActivity : AppCompatActivity() {
+    lateinit var binding: ActivityShowInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityShowInfoBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
     }
